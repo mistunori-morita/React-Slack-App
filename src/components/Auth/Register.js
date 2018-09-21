cf07400b34b8c08b6d17a034d175b58c280c8082
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-09-21 12:58:05 +0900

    Add style

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
+import { Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react'
+import { Link } from 'react-router-dom' 
 
 export default class Register extends Component {
+
+  state = {}
+
+  handleChange = () => {}
+
+
+
+
   render() {
-    return (
-      <div>
-        Register
-      </div>
-    )
+    return <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="address book outline" color="orange" />
+            DevChat
+          </Header>
+          <Form size="lage">
+            <Segment stacked>
+              <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" type="text" onChange={this.handleChange} />
+              <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email Address" type="email" onChange={this.handleChange} />
+              <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" type="password" onChange={this.handleChange} />
+              <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation" type="password" onChange={this.handleChange} />
+              <Button color="orange" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already a user? <Link to="/login">Login</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>;
   }
 }
