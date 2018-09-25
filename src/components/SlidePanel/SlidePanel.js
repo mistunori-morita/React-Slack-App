755e6fda0875ee518d0d7c44c70f7eca283c8ac4
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-09-25 12:53:04 +0900

    Add App Components

---
+++ src/components/SlidePanel/SlidePanel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SlidePanel extends Component {
+  render() {
+    return (
+      <div>
+        SlidePanel
+      </div>
+    )
+  }
+}
