09c33d657c8aa1ef8a5c23869a76f4db042264c4
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-09-25 13:09:04 +0900

    Add DropDown menu

--- src/components/SlidePanel/SlidePanel.js
+++ src/components/SlidePanel/SlidePanel.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import UserPanel from './UserPanel'
+import { Menu } from 'semantic-ui-react'
 
 export default class SlidePanel extends Component {
   render() {
     return (
-      <div>
-        SlidePanel
-      </div>
+      <Menu 
+      size="large"
+        inverted
+        fixed="left"
+        vertical
+        style={{background: '#4c3c4c', fontSize: '1.2rem'}}
+      >
+        <UserPanel />
+      </Menu>
     )
   }
 }
