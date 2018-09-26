3070e87f826ac5b3a40dbcca1508787635f180b5
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-09-26 13:22:03 +0900

    Add Channels DB colections

--- src/components/SlidePanel/Channels.js
+++ src/components/SlidePanel/Channels.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react'
 import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react'
-
+import firebase from '../../firebase'
 
 export default class Channels extends Component {
 
   state = {
+    user: this.props.currentUser,
     channels: [],
     channelName: '',
     channelDetails: '',
+    channelsRef: firebase.database().ref('channels'),
     modal: false
   }
+
+
+  addChannel = () => {
+    const { channelsRef, channelName, channelDetails, user } = this.state;
+
+    const key = channelsRef.push().key
+
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: user.displayName,
+        avatar: user.photoURL
+      }
+    };
+
+    channelsRef
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        this.setState({ channelName: "", channelDetails: '' });
+        this.closeModal()
+        console.log('channel Added')
+      })
+      .catch(err => {
+        console.error(err);
+      })
+
+  }
+
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if(this.isFormValid(this.state)){
+      this.addChannel();
+    }
+  }
+
+
+  isFormValid = ({ channelName, channelDetails }) => channelName && channelDetails;
+
 
   openModal = () => {
     this.setState({
@@ -46,7 +90,7 @@ export default class Channels extends Component {
         <Modal basic open={modal} onClose={this.closeModal}>
           <Modal.Header>Add a Channle</Modal.Header>
           <Modal.Content>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Form.Field>
                 <Input fluid label="Name of Channel" name="channelName" onChange={this.handleChange} />
               </Form.Field>
@@ -57,7 +101,7 @@ export default class Channels extends Component {
           </Modal.Content>
 
           <Modal.Actions>
-            <Button color="green" inverted>
+            <Button color="green" inverted onClick={this.handleSubmit}>
               <Icon name="checkmark" /> Add
             </Button>
             <Button color="red" inverted onClick={this.closeModal}>
--- src/components/SlidePanel/SlidePanel.js
+++ src/components/SlidePanel/SlidePanel.js
@@ -15,7 +15,7 @@ export default class SlidePanel extends Component {
         style={{background: '#4c3c4c', fontSize: '1.2rem'}}
       >
         <UserPanel currentUser={currentUser} />
-        <Channels />
+        <Channels currentUser={currentUser}/>
       </Menu>
     )
   }
